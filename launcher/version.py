abc9251ea0faeaef60477341ab3a94587cfa2900
Jan Krupa <apc.atari@gmail.com>
2023-08-11 22:47:22 +0200

    bump version

--- launcher/version.py
+++ launcher/version.py
@@ -1,12 +1,12 @@
 NAME = "FujiNet-PC Launcher"
 
-VERSION_MAJOR = "2307"
+VERSION_MAJOR = "2308"
 VERSION_MINOR = "0"
 VERSION_BUILD = "ccb6a23"
-VERSION_DATE = "2023-07-17 00:00:00"
-VERSION_FULL = "2307.0.ccb6a23"
+VERSION_DATE = "2023-08-11 00:00:00"
+VERSION_FULL = "2308.0.ccb6a23"
 
 DESCRIPTION = "A launcher to control FujiNet-PC and NetSIO hub."
 
-COPYRIGHT = "(C) 2022 apc"
+COPYRIGHT = "(C) 2022-2023 apc"
 WEBSITE = "https://github.com/a8jan/fujinet-pc-launcher"
